8954e398fde9d3c07acd5febc9adb09fb1287781
Nadjafovv <orxannadjafov29@gmail.com>
2024-07-12 20:32:34 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+ //1 задание
+
+//alert('привет, ' + prompt('Введите свое имя?'));
+
+//2 задание 
+
+// const year = 2024
+// const question = prompt('Введите год рождения')
+// const quest = Number(question)
+
+//  alert(year - quest);
+
+//3 задание
+
+// const square = prompt('Введите длину стороны квадрата')
+// const digit = Number(square)
+
+// alert(digit * 4)
+
+//задание 4
+
+// const radius = prompt('Введите длину радиуса окружности')
+// const digit = Number(radius)
+// const pi = 3
+
+// alert(radius * radius * pi)
+
+//задание 5
+
+// const distance = prompt('Введите расстояние')
+// const digit1 = Number(distance)
+
+// const time = prompt('Введите время за которое хотите добраться')
+// const digit2 = Number(time)
+
+// alert(digit1 * digit2 +'км/ч')
+
+//6 задание
+
+// const money = prompt('Введите количество долларов')
+// const dollar = Number(money)
+// const euro = 0.92
+
+// alert(dollar * euro +'€')
+
+//7 задание
+
+// const memory = +prompt('Укажите объем флешки')
+// const gb = 1024
+
+// alert(memory * gb / 820)
+
+//8 задание
+
+// const pocketmoney = +prompt('Выворачивай карманы')
+// const chocoprice = +prompt('цену назови, воин')
+
+// alert(pocketmoney / chocoprice + ' choco boys')
+// alert(pocketmoney % chocoprice + ' bucks')
+
+//9 exercise
+
+// nmb = prompt('Введите трехзначное число: ');
+// const a = Number(nmb % 10);
+// const b = Number((nmb - a) / 10 % 10);
+// const c = Number((nmb - a - b * 10) / 100);
+// alert('Перевертыш ' + (a * 100 + b * 10 + c));
